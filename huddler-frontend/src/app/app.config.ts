c75eeb19dfed316357311fb3740a38caeb8d13ad
AryushiK <aryushi@huddler.in>
2025-04-12 03:55:10 +0530

    Created huddler landing page

---
+++ huddler-frontend/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideClientHydration(withEventReplay()),
+  ],
+};
